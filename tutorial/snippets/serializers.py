0b1b6f8073cd8c424afecec9d0a6da614d65c3cc
hnkisadiq <hnkisadiq@gmail.com>
2017-10-11 14:52:12 +0530

    Update serializers.py

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from snippets.models import Farms
-from snippets.models import Houses
+from snippets.models import Houses,Well
 from snippets.models import Snippet,Identity,Property_1,Property_2, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
@@ -90,7 +90,7 @@ class Property2Serializer(serializers.ModelSerializer):
 class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Houses
-        fields = ('id','H_id', 'lat', 'lon','Area','Income')
+        fields = ('id','H_id', 'h_point','Area','Income')
 
     def create(self, validated_data):
         """
@@ -103,10 +103,29 @@ class HouseSerializer(serializers.ModelSerializer):
         Update and return an existing `Snippet` instance, given the validated data.
         """
         instance.H_id = validated_data.get('H_id', instance.H_id)
-        instance.lat = validated_data.get('lat', instance.lat)
-        instance.long = validated_data.get('long', instance.long)
+        instance.h_point = validated_data.get('h_point', instance.h_point)
         instance.Area = validated_data.get('Area', instance.Area)
         instance.Income = validated_data.get('Income', instance.Income)
+        instance.save()
+        return instance
+class WellSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Well
+        fields = ('id','w_id', 'w_point','depth')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Well.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.w_id = validated_data.get('w_id', instance.w_id)
+        instance.w_point = validated_data.get('w_point', instance.w_point)
+        instance.depth = validated_data.get('depth', instance.depth)
         instance.save()
         return instance
 
@@ -126,14 +145,6 @@ class FarmSerializer(serializers.ModelSerializer):
         Update and return an existing `Snippet` instance, given the validated data.
         """
         instance.F_id = validated_data.get('F_id', instance.F_id)
-        '''instance.lat1 = validated_data.get('lat1', instance.lat1)
-        instance.long1 = validated_data.get('long1', instance.long1)
-        instance.lat2 = validated_data.get('lat2', instance.lat2)
-        instance.long2 = validated_data.get('long2', instance.long2)
-        instance.lat3 = validated_data.get('lat3', instance.lat3)
-        instance.long3 = validated_data.get('long3', instance.long3)
-        instance.lat4 = validated_data.get('lat4', instance.lat4)
-        instance.long4 = validated_data.get('long4', instance.long4)'''
         instance.poly = validated_data.get('poly', instance.poly)
         instance.Area = validated_data.get('Area', instance.Area)
         instance.crops = validated_data.get('crops', instance.crops)
