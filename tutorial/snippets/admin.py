cbed3a92d94bbe7a06e30ecda9539f5d9a9c3cd1
hnkisadiq <hnkisadiq@gmail.com>
2017-10-11 14:57:28 +0530

    Update admin.py

--- tutorial/snippets/admin.py
+++ tutorial/snippets/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.gis import admin
 # Register your models here.
-from .models import Snippet,Houses,Farms,Property_1,Property_2,Identity
+from .models import Snippet,Houses,Farms,Property_1,Property_2,Identity,Well
 
-admin.site.register(Houses)
+admin.site.register(Houses, admin.GeoModelAdmin)
+admin.site.register(Well, admin.GeoModelAdmin)
 admin.site.register(Identity)
 admin.site.register(Farms, admin.GeoModelAdmin)
 admin.site.register(Property_1)
